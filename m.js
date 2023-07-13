42981ad4e88596a3d234bf0c4928f27c7981b1a0
Romain Parent <125014681+audirp29@users.noreply.github.com>
2023-07-14 01:10:10 +0200

    Update m.js

--- m.js
+++ m.js
@@ -69,35 +69,65 @@ $(document).ready(function() {
               city: data.name,
               temperature: Math.round(data.main.temp - 273.15), // Convertir de Kelvin à Celsius
               humidity: data.main.humidity,
-        conditions: data.weather[0].description
+              conditions: data.weather[0].description
             };
-        
             var addautoo = '<h2 style="color: rgb(255, 0, 220);">' + weather.city + '</h2>';
             addautoo += '<h3 style="color: rgb(255, 0, 220);">Temperature: ' + weather.temperature + '&deg;C</h3>';
             addautoo += '<h3 style="color: rgb(255, 0, 220);">Humidity: ' + weather.humidity + '%</h3><br>';
             addautoo += '<h3 style="color: rgb(255, 0, 220);">Conditions: ' + weather.conditions + '</h3>';
-      
-      
+          
             $('#wauto').html(addautoo);
+          
+            var uvUrll = 'https://api.openweathermap.org/data/2.5/uvi?appid=67650783563069701696bbe0037ffe19&lat=' + data.coord.lat + '&lon=' + data.coord.lon;
+          
+            $.getJSON(uvUrll, function(uvData) {
+              var uvIndex = uvData.value;
+              var uvRange = '';
+              if (uvIndex >= 0 && uvIndex <= 5) {
+                uvRange = '0 a 5 good';
+              } else if (uvIndex >= 6 && uvIndex <= 10) {
+                uvRange = '6 a 10 hot';
+              } else {
+                uvRange = '11 a + burn';
+              }
+              var uvHtmll = '<h3 style="color: rgb(255, 0, 220);">UV Index: ' + uvIndex + '</h3>';
+              uvHtmll += '<h3 style="color: rgb(255, 0, 220);">UV Range: ' + uvRange + '</h3>';
+              $('#wauto').append(uvHtmll);
+            }).fail(function() {
+              console.log("Erreur lors de la récupération de l'indice UV.");
+            });
           });
-    });
+        });
+          
+
 });
 
-        $.getJSON(apiUrlll, function(data) {
-            var weather = {
-              city: data.name,
-              temperature: Math.round(data.main.temp - 273.15), // Convertir de Kelvin à Celsius
-              humidity: data.main.humidity,
-        conditions: data.weather[0].description
-            };
-        
-            var weatherHtmlll = '<h2 style="color: rgb(255, 0, 0);">' + weather.city + '</h2>';
-            weatherHtmlll += '<h3 style="color: rgb(255, 0, 0);">Temperature: ' + weather.temperature + '&deg;C</h3>';
-            weatherHtmlll += '<h3 style="color: rgb(255, 0, 0);">Humidity: ' + weather.humidity + '%</h3><br>';
-            weatherHtmlll += '<h3 style="color: rgb(255, 0, 0);">Conditions: ' + weather.conditions + '</h3>';
-      
-      
-            $('#weatherrr').html(weatherHtmlll);
-    });
+
+$.getJSON(apiUrlll, function(data) {
+  var weather = {
+      city: data.name,
+      temperature: Math.round(data.main.temp - 273.15), // Convertir de Kelvin à Celsius
+      humidity: data.main.humidity,
+      conditions: data.weather[0].description
+  };
+
+  var weatherHtmlll = '<h2 style="color: rgb(255, 0, 0);">' + weather.city + '</h2>';
+  weatherHtmlll += '<h3 style="color: rgb(255, 0, 0);">Temperature: ' + weather.temperature + '&deg;C</h3>';
+  weatherHtmlll += '<h3 style="color: rgb(255, 0, 0);">Humidity: ' + weather.humidity + '%</h3><br>';
+  weatherHtmlll += '<h3 style="color: rgb(255, 0, 0);">Conditions: ' + weather.conditions + '</h3>';
+
+  $('#weatherrr').html(weatherHtmlll);
+
+  var uvUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=67650783563069701696bbe0037ffe19&lat=' + data.coord.lat + '&lon=' + data.coord.lon;
+
+  $.getJSON(uvUrl, function(uvData) {
+      var uvIndex = uvData.value;
+      var uvHtml = '<h3 style="color: rgb(255, 0, 0);">UV Index: ' + uvIndex + '</h3>';
+      $('#weatherrr').append(uvHtml);
+  }).fail(function() {
+      console.log("Erreur lors de la récupération de l'indice UV.");
   });
+
+});
+});
 });
